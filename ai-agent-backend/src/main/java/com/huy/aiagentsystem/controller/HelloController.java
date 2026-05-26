2fe6cbe0920ad5ea3201f685e195f14a12392181
HhuyH <hoanhuy12@gmail.com>
2026-05-26 22:57:22 +0700

    test HPA

--- ai-agent-backend/src/main/java/com/huy/aiagentsystem/controller/HelloController.java
+++ ai-agent-backend/src/main/java/com/huy/aiagentsystem/controller/HelloController.java
@@ -10,4 +10,16 @@ public class HelloController {
     public String hello() {
         return "hello v3";
     }
+
+    @GetMapping("/hello/cpu-test")
+    public String cpuTest() {
+
+        long sum = 0;
+
+        for (long i = 0; i < 2_000_000_000L; i++) {
+            sum += i;
+        }
+
+        return "CPU test done: " + sum;
+    }
 }
